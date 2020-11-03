fea9d5f0b030124e9d4aa822fd3e31c17e8cfbd2
Amna Jusić <ajusic5@etf.unsa.ba>
2020-11-03 17:14:12 +0100

    Dodan konstruktor za klasu Interval.java

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -2,5 +2,16 @@ package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
 
+    private double pocetnaTacka;
+    private double krajnjaTacka;
+    private boolean daLiPocetnaTackaPripada;
+    private boolean daLiKrajnjaTackaPripada;
+    public Interval(double pocetna, double krajnja, boolean daLiPocetnaPripada, boolean daLiKrajnjaPripada){
+        pocetnaTacka=pocetna;
+        krajnjaTacka=krajnja;
+        daLiPocetnaTackaPripada=daLiPocetnaPripada;
+        daLiKrajnjaTackaPripada=daLiKrajnjaPripada;
+    }
+
 
 }
