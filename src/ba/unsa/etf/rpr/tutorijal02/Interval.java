0fc552439f076005ae2341980e92d66d915c90d4
Amna Jusić <ajusic5@etf.unsa.ba>
2020-11-03 17:19:30 +0100

    Dodano bacanje izuzetka u konstruktoru klase Interval.java

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -6,12 +6,15 @@ public class Interval {
     private double krajnjaTacka;
     private boolean daLiPocetnaTackaPripada;
     private boolean daLiKrajnjaTackaPripada;
-    public Interval(double pocetna, double krajnja, boolean daLiPocetnaPripada, boolean daLiKrajnjaPripada){
+    public Interval(double pocetna, double krajnja, boolean daLiPocetnaPripada, boolean daLiKrajnjaPripada) {
+
+        if(krajnja>pocetna) throw new IllegalArgumentException("Neispravna vrijednost tacaka");
         pocetnaTacka=pocetna;
         krajnjaTacka=krajnja;
         daLiPocetnaTackaPripada=daLiPocetnaPripada;
         daLiKrajnjaTackaPripada=daLiKrajnjaPripada;
     }
+
 
 
 }
